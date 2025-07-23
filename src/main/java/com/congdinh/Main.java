46638243c159ce31620daf757b1878dd5c688d6f
Cong Dinh <congdinh2012@hotmail.com>
2025-07-23 15:04:29 +0700

    Hibernate configuration and add hibernate.cfg.xml for database settings

--- src/main/java/com/congdinh/Main.java
+++ src/main/java/com/congdinh/Main.java
@@ -1,7 +1,22 @@
 package com.congdinh;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("=============Check Hibernate Configuration=============");
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        System.out.println("Hibernate session opened successfully!");
+        session.getTransaction().commit();
+        session.close();
+        sessionFactory.close();
     }
 }
