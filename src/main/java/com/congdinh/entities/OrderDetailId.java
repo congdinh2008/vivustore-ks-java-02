0eb783a3a29f1f4da88bf716904eac443188bb2c
Cong Dinh <congdinh2012@hotmail.com>
2025-07-28 16:23:23 +0700

    Refactor OrderDetailId class to implement Serializable and add JPA annotations for embedding

--- src/main/java/com/congdinh/entities/OrderDetailId.java
+++ src/main/java/com/congdinh/entities/OrderDetailId.java
@@ -1,5 +1,39 @@
 package com.congdinh.entities;
 
-public class OrderDetailId {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class OrderDetailId implements Serializable {
+    private int orderId;
+
+    private int productId;
+
+    // Default constructor
+    public OrderDetailId() {
+    }
+
+    // Parameterized constructor
+    public OrderDetailId(int orderId, int productId) {
+        this.orderId = orderId;
+        this.productId = productId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 }
